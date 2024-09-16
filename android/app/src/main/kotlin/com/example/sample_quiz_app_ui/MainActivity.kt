aed72e6e925295e80497fe0f57b8252d8d317f7a
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-16 16:07:29 +0300

    Create project file

---
+++ android/app/src/main/kotlin/com/example/sample_quiz_app_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sample_quiz_app_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SampleQuizApp());
+}
+
+class SampleQuizApp extends StatelessWidget {
+  const SampleQuizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
