6e593b8212672eb305ff114939180f964ece412e
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-17 01:41:42 +0300

    update on ActionHome widget

--- lib/views/widgets/action_home_widget.dart
+++ lib/views/widgets/action_home_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sample_quiz_app_ui/views/create_quiz_view.dart';
 import 'package:sample_quiz_app_ui/views/widgets/custom_elevated_button_widget.dart';
 
 class ActionHomeWidget extends StatelessWidget {
@@ -8,7 +9,11 @@ class ActionHomeWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        CustomElevatedButtonWidget(textName: "Create Quiz", onPressed: () {}),
+        CustomElevatedButtonWidget(
+            textName: "Create Quiz",
+            onPressed: () {
+              Navigator.pushNamed(context, const CreateQuizView().id);
+            }),
         const SizedBox(
           height: 12,
         ),
