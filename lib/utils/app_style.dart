27cc9dba955037f09954c4d1be89b362b105040f
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-17 01:27:59 +0300

    Add textstyles on AppStyle

--- lib/utils/app_style.dart
+++ lib/utils/app_style.dart
@@ -12,4 +12,28 @@ abstract class AppStyle {
     fontSize: 28,
     fontWeight: FontWeight.w500,
   );
+
+  static const TextStyle styleMedium18 = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle styleRegular18 = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle styleBold22 = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 22,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle styleSemiBold28 = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 28,
+    fontWeight: FontWeight.w600,
+  );
 }
