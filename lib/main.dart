11756fcbcb3adea1c3b2bcf7d2f9da72203630b1
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-18 03:13:54 +0300

    Update on main register all Adapters and Open all Boxes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:sample_quiz_app_ui/models/models/answer_model/answer_model.dart';
+import 'package:sample_quiz_app_ui/models/models/question_model/question_model.dart';
+import 'package:sample_quiz_app_ui/models/models/quiz_model/quiz_model.dart';
+import 'package:sample_quiz_app_ui/models/models/taked_quiz_model/taked_quiz_model.dart';
+import 'package:sample_quiz_app_ui/utils/constants.dart';
 import 'package:sample_quiz_app_ui/views/create_quiz_view.dart';
 import 'package:sample_quiz_app_ui/views/home_view.dart';
 
-void main() {
+void main() async {
+  Hive.registerAdapter(AnswerModelAdapter());
+  Hive.registerAdapter(QuestionModelAdapter());
+  Hive.registerAdapter(QuizModelAdapter());
+  Hive.registerAdapter(TakedQuizModelAdapter());
+
+  await Hive.openBox<AnswerModel>(kAnswerBox);
+  await Hive.openBox<QuestionModel>(kQuestionBox);
+  await Hive.openBox<QuizModel>(kQuizBox);
+  await Hive.openBox<TakedQuizModel>(kTakedQuizBox);
   runApp(const SampleQuizApp());
 }
 
