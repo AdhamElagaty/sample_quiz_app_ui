da067e005a988896c4abc7b12f06459d89b1bf0f
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-18 03:24:03 +0300

    Create QuizController

---
+++ lib/controllers/quiz_controller.dart
@@ -0,0 +1,31 @@
+import 'package:sample_quiz_app_ui/models/models/quiz_model/quiz_model.dart';
+import 'package:sample_quiz_app_ui/models/repositories/quiz_repository/iquiz_repository.dart';
+import 'package:sample_quiz_app_ui/models/repositories/quiz_repository/quiz_repository.dart';
+
+class QuizController {
+  late final IQuizRepository _repository;
+
+  QuizController() {
+    _repository = QuizRepository();
+  }
+
+  Future<void> addQuiz(QuizModel quiz) async {
+    await _repository.addQuiz(quiz);
+  }
+
+  Future<void> updateQuiz(QuizModel quiz) async {
+    await _repository.updateQuiz(quiz);
+  }
+
+  Future<void> deleteQuiz(int id) async {
+    await _repository.deleteQuiz(id);
+  }
+
+  Future<List<QuizModel>> getAllQuizzes() async {
+    return await _repository.getAllQuizzes();
+  }
+
+  Future<QuizModel?> getQuizById(int id) async {
+    return await _repository.getQuizById(id);
+  }
+}
