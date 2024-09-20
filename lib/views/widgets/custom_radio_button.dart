9c7aa6ae3c035fbe6cba2a44faada9c88da3684d
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-21 01:51:23 +0300

    Craete Genric CustomRadioButton

---
+++ lib/views/widgets/custom_radio_button.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CustomRadioButton<T> extends StatelessWidget {
+  final Widget child;
+  final T value;
+  final T groupValue;
+  final ValueChanged<T?> onChanged;
+  final Color activeColor;
+  final Color inactiveColor;
+  final double size;
+
+  const CustomRadioButton({
+    super.key,
+    required this.child,
+    required this.value,
+    required this.groupValue,
+    required this.onChanged,
+    this.activeColor = Colors.green,
+    this.inactiveColor = Colors.grey,
+    this.size = 24.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    bool isSelected = value == groupValue;
+
+    return GestureDetector(
+      onTap: () {
+        onChanged(value);
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        alignment: Alignment.topCenter,
+        constraints:
+            BoxConstraints(maxWidth: MediaQuery.of(context).size.width),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+            color: isSelected ? activeColor : inactiveColor,
+            width: isSelected ? 4.0 : 2.0,
+          ),
+          color: isSelected ? activeColor.withOpacity(0.2) : Colors.transparent,
+        ),
+        child: child,
+      ),
+    );
+  }
+}
