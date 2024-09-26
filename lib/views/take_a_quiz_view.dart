1b50c3f828022264e35580a4665bbc4b022d59ae
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-26 16:36:57 +0300

    Update on TakeAQuizView

--- lib/views/take_a_quiz_view.dart
+++ lib/views/take_a_quiz_view.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:sample_quiz_app_ui/views/cubits/task_quiz_cubit/task_quiz_cubit.dart';
-import 'package:sample_quiz_app_ui/views/cubits/task_quiz_cubit/task_quiz_state.dart';
+import 'package:sample_quiz_app_ui/models/models/quiz_model/quiz_model.dart';
+import 'package:sample_quiz_app_ui/views/cubits/task_quiz_cubit/take_quiz_cubit.dart';
+import 'package:sample_quiz_app_ui/views/cubits/task_quiz_cubit/take_quiz_state.dart';
 import 'package:sample_quiz_app_ui/views/widgets/complete_take_a_quiz_view_body.dart';
 import 'package:sample_quiz_app_ui/views/widgets/question_take_a_quiz_view_body.dart';
 import 'package:sample_quiz_app_ui/views/widgets/view_answers_take_a_quiz_view_body.dart';
 import 'package:sample_quiz_app_ui/views/widgets/welcome_take_a_quiz_view_body.dart';
 
 class TakeAQuizView extends StatelessWidget {
-  const TakeAQuizView({super.key});
+  const TakeAQuizView({
+    super.key,
+    required this.quizModel,
+    this.imageTag,
+  });
 
   final String id = "TakeAQuizView";
+  final QuizModel quizModel;
+  final int? imageTag;
 
   @override
   Widget build(BuildContext context) {
@@ -28,20 +35,32 @@ class TakeAQuizView extends StatelessWidget {
               }
             },
             builder: (context, state) {
-              return SingleChildScrollView(child: bodyView(state));
+              if (state is TakeQuizInitial) {
+                BlocProvider.of<TakeQuizCubit>(context)
+                    .fetchQuizLazy(quizModel.key);
+                BlocProvider.of<TakeQuizCubit>(context).takedQuizModel.quizId =
+                    quizModel.key;
+              }
+              return SingleChildScrollView(child: bodyView(state, imageTag));
             },
           )),
     );
   }
 
-  Widget bodyView(TakeQuizState state) {
-    if (state is TakeQuizInitial) {
-      return const WelcomeTakeAQuizViewBody();
+  Widget bodyView(TakeQuizState state, int? imageTag) {
+    if (state is TakeQuizInitial || state is ErrorInNameValidation) {
+      return WelcomeTakeAQuizViewBody(
+        imageTag: imageTag,
+      );
     } else if (state is CompleteQuiz) {
-      return const CompleteTakeAQuizViewBody();
+      return CompleteTakeAQuizViewBody(
+        imageTag: imageTag,
+      );
     } else if (state is ViewRightAnswersQuiz ||
         state is GetNextOrLastRightAnswer) {
-      return const ViewAnswersTakeAQuizViewBody();
+      return ViewAnswersTakeAQuizViewBody(
+        imageTag: imageTag,
+      );
     } else {
       return const QuestionTakeAQuizViewBody();
     }
